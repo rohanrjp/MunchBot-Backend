7d7fb091bd17ca8c863ed358f017c231c1883dd3
Rohan <rohan1007rjp@gmail.com>
2025-05-12 22:46:08 +0530

    Fix for EOF issue

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .config import settings
 
-engine=create_engine(url=settings.DB_STRING)
+engine=create_engine(url=settings.DB_STRING,pool_size=20,max_overflow=0,pool_timeout=30,pool_recycle=3600)
 SessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
 Base=declarative_base()
 
